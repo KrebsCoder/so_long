0ed3b62b86ea12a013244a072062ad6e1398573c
KrebsCoder <lucasklopess@gmail.com>
2022-01-20 19:07:29 +0100

    update on main

--- src/so_long.c
+++ src/so_long.c
@@ -6,25 +6,23 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 23:45:37 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/19 01:44:33 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/20 05:01:19 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <so_long.h>
+#include "so_long.h"
 
-int so_long(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	t_game game;
 
 	if (argc == 2)
-	{
-		game.data_map.map = map_gen(argv[1]);
-	}
+		game.data_map.map = map_gen(game, argv[1]);
 	else
 		write(1, "Error\n, map is missing", 23);
-	if (map_check(&game.data_map.map, argv[1]))
+	if (map_check(&game, argv[1]))
 	{
-		// init nas coisas
+		printf("Seems fine");
 	}
-		//...
+	return (1);
 }
