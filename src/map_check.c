f3834e507914b749c918ea0c2ce4f4c3eac2c0b9
KrebsCoder <lucasklopess@gmail.com>
2022-01-21 17:55:35 +0100

    fixed map_check

--- src/map_check.c
+++ src/map_check.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 23:25:32 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/20 19:03:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/21 17:20:50 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 int	map_check(t_game *game, char *file)
 {
+	t_map_initialize(game);
 	if (count_wall(game), validate_wall(game), validate_extension(file),
-	 validate_content(game))
+		validate_content(game))
 		return (1);
 	else
+	{
+		printf("Error\nInvalid map.\n");
 		return (0);
+	}
 }
 
 int	count_wall(t_game *game)
 {
-	int column;
-	int row;
-	int j;
+	int	column;
+	int	row;
+	int	j;
 
 	j = ft_strlen(game->data_map.map[0]);
-
 	row = 0;
 	while (game->data_map.map[row])
 	{
@@ -45,33 +48,28 @@ int	count_wall(t_game *game)
 	return (1);
 }
 
-int validate_wall(t_game *game)
+int	validate_wall(t_game *game)
 {
-	int column;
-	int row;
-	int row_size;
-	int column_size;
+	int	column;
+	int	row;
+	int	row_size;
+	int	column_size;
 
-	column_size = 0;
 	row_size = 0;
-	while(game->data_map.map[row_size])
+	while (game->data_map.map[row_size])
 		row_size++;
-	column_size  = ft_strlen(game->data_map.map[0]);
-
+	column_size = ft_strlen(game->data_map.map[0]);
 	row = 0;
-	while(row < row_size)
+	while (row < row_size)
 	{
 		column = 0;
-		while(column < column_size)
+		while (column < column_size)
 		{
 			if (game->data_map.map[0][column] != '1' ||
 			game->data_map.map[row_size - 1][column] != '1' ||
 			game->data_map.map[row][0] != '1' ||
 			game->data_map.map[row][column_size - 1] != '1')
-			{
-				printf("Error\nMap is invalid.");
 				return (0);
-			}
 			column++;
 		}
 		row++;
@@ -81,28 +79,30 @@ int validate_wall(t_game *game)
 
 int	validate_extension(char *file)
 {
-	char *str;
+	char	*str;
 
 	if (!file)
 		return (0);
-
 	str = ft_strchr(file, '.');
 	if (ft_strncmp(str, ".ber", 5))
 		return (1);
 	else
+	{
+		printf("invalid extension\n");
 		return (0);
+	}
 }
 
 int	validate_content(t_game *game)
 {
-	int row;
-	int column;
+	int	row;
+	int	column;
 
 	row = 0;
-	while(game->data_map.map[row])
+	while (game->data_map.map[row])
 	{
 		column = 0;
-		while(game->data_map.map[row][column])
+		while (game->data_map.map[row][column])
 		{
 			if (game->data_map.map[row][column] == 'C')
 				game->data_map.collectables_count++;
@@ -114,9 +114,8 @@ int	validate_content(t_game *game)
 		}
 		row++;
 	}
-	if (game->data_map.collectables_count == 0 || 
-	game->data_map.exit_count != 1 || game->data_map.player_count != 1)
+	if (game->data_map.collectables_count == 0
+		|| game->data_map.exit_count != 1 || game->data_map.player_count != 1)
 		return (0);
-	else
-		return (1);
+	return (1);
 }
