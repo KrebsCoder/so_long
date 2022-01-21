3689c6d49b59aeb2bbcad65c8084a9ba6926d188
lkrebs-l <lkrebs-l@student.42sp.org.br>
2022-01-21 17:55:55 +0100

    Added macro of sprites

--- src/so_long.h
+++ src/so_long.h
@@ -6,13 +6,13 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 23:45:34 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/20 17:19:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/21 17:55:02 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include "../libraries/mlx/mlx.h"
+# include <mlx.h>
 # include "../libraries/libft/libft.h"
 
 # include <stdio.h>
@@ -22,32 +22,50 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
+#define PLAYER_DOWN "sprites/Player_Down.xpm"
+#define PLAYER_LEFT "sprites/Player_Left.xpm"
+#define PLAYER_UP "sprites/Player_Up.xpm"
+#define PLAYER_RIGHT "sprites/Player_right.xpm"
+#define WALL "sprites/Wall.xpm"
+#define GOAL_BLOCKED "sprites/Goal_Blocked.xpm"
+#define GOAL_OPEN "sprites/Goal_Open.xpm"
+#define CHEESE "sprites/Collect.xpm"
+#define BACKGROUND "sprites/Background.xpm"
+
 typedef struct s_map
 {
-	char **map;
-	int collum;
-	int row;
-	int player_count;
-	int exit_count;
-	int collectables_count;
+	char 	**map;
+	int 	collum;
+	int 	row;
+	int 	player_count;
+	int 	exit_count;
+	int 	collectables_count;
 } t_map;
 
 typedef struct s_game
 {
-	void *init;
-	void *win;
-	void *player_w;
-	void *player_s;
-	void *player_d;
-	void *player_a;
-	t_map data_map;
+	void 	*mlx;
+	void 	*win;
+	int		win_height;
+	int		win_width;
+	void 	*player_w;
+	void 	*player_s;
+	void 	*player_d;
+	void 	*player_a;
+	int 	collected_cheese;
+	int 	moves_count;
+	t_map 	data_map;
 } t_game;
 
-char	**map_gen(t_game game, char *file);
+char	**map_gen(t_game *game, char *file);
 int		map_check(t_game *game, char *file);
 int		count_wall(t_game *game);
 int 	validate_wall(t_game *game);
 int		validate_extension(char *file);
 int		validate_content(t_game *game);
+void 	new_win(t_game game);
+void	game_init(t_game *game);
+void	t_map_initialize(t_game *game);
+void 	data_init(t_game *game);
 
 #endif
