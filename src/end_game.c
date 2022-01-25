f1371d32bc2822246259460feb140e4a7999b71c
KrebsCoder <lucasklopess@gmail.com>
2022-01-25 20:33:07 +0100

    new file

---
+++ src/end_game.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   end_game.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 20:32:32 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2022/01/25 20:32:47 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	free_ptr(void *ptr)
+{
+	free(ptr);
+	ptr = NULL;
+}
+
+void free_map(t_game *game)
+{
+	int column;
+
+	column = 0;
+	while(game->data_map.map[column])
+	{
+		free_ptr(game->data_map.map[column]);
+		column++;
+	}
+}
+
+void exit_game(t_game *game)
+{
+	mlx_destroy_image(game->mlx, game->p_down);
+	mlx_destroy_image(game->mlx, game->p_up);
+	mlx_destroy_image(game->mlx, game->p_left);
+	mlx_destroy_image(game->mlx, game->p_right);
+	mlx_destroy_image(game->mlx, game->cheese);
+	mlx_destroy_image(game->mlx, game->wall);
+	mlx_destroy_image(game->mlx, game->background);
+	mlx_destroy_image(game->mlx, game->exit_closed);
+	mlx_destroy_image(game->mlx, game->exit_open);
+	mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
+	free_ptr(game->mlx);
+	free_map(game);
+}
