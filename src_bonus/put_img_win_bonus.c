8248dd7254fa7d1bde5bdb2d9b09b2be54cbe602
lkrebs-l <lkrebs-l@student.42sp.org.br>
2022-01-26 22:40:32 +0100

    Bonus and Mandatory is working without leaks

--- src_bonus/put_img_win_bonus.c
+++ src_bonus/put_img_win_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 19:20:18 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/26 01:52:09 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/26 19:19:24 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 int	put_img_win(t_game *game)
 {
-	int	row;
-	int	column;
+	int		row;
+	int		column;
+	char	*moves;
 
 	column = 0;
 	while (game->data_map.map[column])
@@ -23,21 +24,48 @@ int	put_img_win(t_game *game)
 		row = 0;
 		while (game->data_map.map[column][row])
 		{
-			if (game->data_map.map[column][row] == '1')
-				put_img(game, column, row, game->wall);
-			else if (game->data_map.map[column][row] == '0')
-				put_img(game, column, row, game->background);
-			else if (game->data_map.map[column][row] == 'E')
-				put_img(game, column, row, game->exit_closed);
-			else if (game->data_map.map[column][row] == 'C')
-				put_img(game, column, row, game->cheese);
-			else
-				put_img(game, column, row, game->p_down);
+			put_img_win_bonus(game, column, row);
+			moves = ft_itoa(game->moves_count);
+			mlx_string_put(game->mlx, game->win, 25, 25, 0x000000, "MOVES: ");
+			mlx_string_put(game->mlx, game->win, 75, 25, 0x000000, moves);
+			free_ptr(moves);
 			row++;
 		}
 		column++;
 	}
 	return (0);
+}
+
+int	put_img_win_bonus(t_game *game, int column, int row)
+{
+	if (game->data_map.map[column][row] == '1')
+		put_img(game, column, row, game->wall);
+	else if (game->data_map.map[column][row] == '0')
+		put_img(game, column, row, game->background);
+	else if (game->data_map.map[column][row] == 'E'
+		&& game->data_map.collectables_count == 0)
+		put_img(game, column, row, game->exit_open);
+	else if (game->data_map.map[column][row] == 'E')
+		put_img(game, column, row, game->exit_closed);
+	else if (game->data_map.map[column][row] == 'C')
+		put_img(game, column, row, game->cheese);
+	else if (game->data_map.map[column][row] == 'K')
+		put_img(game, column, row, game->enemy_down);
+	else
+		handle_p_direction(game, column, row);
+	return (0);
+}
+
+void	handle_p_direction(t_game *game, int column, int row)
+{
+	if (game->p_direction == 'w')
+		put_img(game, column, row, game->p_up);
+	if (game->p_direction == 's')
+		put_img(game, column, row, game->p_down);
+	if (game->p_direction == 'd')
+		put_img(game, column, row, game->p_right);
+	if (game->p_direction == 'a')
+		put_img(game, column, row, game->p_left);
 }
 
 void	put_img(t_game *game, int row, int column, void *img)
--- src_bonus/so_long_bonus.c
+++ src_bonus/so_long_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 23:45:37 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/26 01:52:12 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/26 22:16:08 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ int	main(int argc, char *argv[])
 	t_game	game;
 
 	if (argc == 2)
+	{
 		game.data_map.map = map_gen(argv[1]);
+		if (!game.data_map.map)
+			return (0);
+	}
 	else
-		printf("Error\n map is missing");
+	{
+		printf("Error\nYou need to send two arguments.\n");
+		exit (0);
+	}
 	if (map_check(&game, argv[1]))
 	{
 		data_init(&game);
