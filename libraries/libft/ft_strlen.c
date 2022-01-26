86a46d3306ad22617af4c2d477c54d6dc1530b33
KrebsCoder <lucasklopess@gmail.com>
2022-01-26 22:17:37 +0100

    fix ft_strlen

--- libraries/libft/ft_strlen.c
+++ libraries/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:35:00 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/09/23 00:59:44 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/26 20:08:20 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *string)
 {
 	size_t	i;
 
+	if (!string)
+		return (0);
 	i = 0;
 	while (string[i])
 		i++;
