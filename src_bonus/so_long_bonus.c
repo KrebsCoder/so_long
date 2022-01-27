bda9e1e03585cf0fc34b9a6aac25fb88c77c1b53
KrebsCoder <lucasklopess@gmail.com>
2022-01-27 20:15:07 +0100

    More error handling

--- src_bonus/so_long_bonus.c
+++ src_bonus/so_long_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 23:45:37 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/26 22:16:08 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/27 19:26:08 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ int	main(int argc, char *argv[])
 	{
 		game.data_map.map = map_gen(argv[1]);
 		if (!game.data_map.map)
-			return (0);
+		{
+			printf("Error\nInvalid map\n");
+			return (1);
+		}
 	}
 	else
 	{
 		printf("Error\nYou need to send two arguments.\n");
-		exit (0);
+		exit (1);
 	}
 	if (map_check(&game, argv[1]))
 	{
