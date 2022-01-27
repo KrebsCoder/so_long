740cdf0805553bdfdc5df42420a8304c3afb4eae
KrebsCoder <lucasklopess@gmail.com>
2022-01-27 19:39:49 +0100

    fixed memory leaks

--- src/map_gen.c
+++ src/map_gen.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 20:59:12 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2022/01/27 16:00:53 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2022/01/27 18:49:15 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char	**map_gen(char *file)
 		free_ptr((void *)buffer);
 	}
 	if (check_next_line(map))
-		exit (0);
+		exit (1);
 	gen_map = ft_split(map, '\n');
 	free_ptr((void *)map);
 	close(fd);
